02132b9a4f6f2efdad7d59372f3aee94dc637857
Daniel Proaño Chacon <danieluxury@gmail.com>
2023-09-19 14:49:17 -0500

    update media logic p7

--- updateSong.php
+++ updateSong.php
@@ -1,5 +1,9 @@
 <?php
 $media = $_POST['media'];
+$url = $_POST['url'];
 
-file_put_contents('now_playing.txt', $media);
+$data_to_save = $media . "\n" . $url;
+
+file_put_contents('now_playing.txt', $data_to_save);
+
 ?>
